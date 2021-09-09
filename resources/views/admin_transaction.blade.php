d77ade49b3b6630214d4cf44c1ea88d5b5a2cd0d
Lyka Casilao <lycasilao/2gbox.adnu.edu.ph>
2021-09-09 17:58:32 +0800

    Sept 8 Sixth Commit

--- resources/views/admin_transaction.blade.php
+++ resources/views/admin_transaction.blade.php
@@ -317,17 +317,19 @@
                                             <input type="hidden" name="status" value="Pending">
                                             <label for="cleaner">Cleaner: </label>
                                             <select name="cleaner_id[]" id="cleaner" >
-                                            @foreach($cleaner_data as $key => $cleaner)
+                                            @foreach($cleaner_data as $cleaner)
                                                 <?php 
                                                       $cleaner = Assigned_cleaner::Where('booking_id', '=', $value->booking_id)->Where('status', 'Accepted')->orWhere('status', 'Declined')->get();
-                                                      foreach($cleaner as $key => $id){
-                                                          $user = Cleaner::Where('cleaner_id', $id -> cleaner_id )->value('user_id');
+                                                ?>
+                                                @foreach($cleaner as $id)
+                                                    <?php
+                                                          $user = Cleaner::Where('cleaner_id', $id->cleaner_id )->value('user_id');
                                                           if ( $user != $cleaner->user_id ){
                                                               $userid = $cleaner->user_id;
                                                               $fullname = User::Where('user_id', $userid )->value('full_name');
                                                           }                       
-                                                      }
-                                                ?>
+                                                    ?>
+                                                @endforeach    
                                                 <option  value="{{  $userid }}">{{ $fullname }}</option>
                                                 @endforeach
                                                 
