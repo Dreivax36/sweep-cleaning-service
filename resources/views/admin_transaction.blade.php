cdc2f8759a23dfe8b90d9a40a6c1583d4122049e
Lyka Casilao <lycasilao/2gbox.adnu.edu.ph>
2021-09-09 16:11:07 +0800

    Sept 8 Second Commit

--- resources/views/admin_transaction.blade.php
+++ resources/views/admin_transaction.blade.php
@@ -327,7 +327,7 @@
                                                 
                                                 </select> <br>    
                                                 <?php
-                                                    $total_cleaner --;
+                                                    $total --;
                                                 ?>
                                                 @endwhile
                                                 <br>
