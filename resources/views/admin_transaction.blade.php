40654e2f40c2b3df407d9f8568b790b5f2b0e31a
Lyka Casilao <lycasilao/2gbox.adnu.edu.ph>
2021-10-09 22:42:57 +0800

    October 10 version 1

--- resources/views/admin_transaction.blade.php
+++ resources/views/admin_transaction.blade.php
@@ -474,7 +474,7 @@
                                                             @if($cleaner_data != null)
                                                                 @foreach($bookingSchedule as $key => $cleanerWithSchedule)
                                                                     <?php  
-                                                                        $cleanerID = Assigned_cleaner::Where('booking_id', $cleanerWithSchedule->booking_id)->orWhere('booking_id', $value->booking_id)->Where('status', 'Accepted')->orWhere('status', 'Declined')->orWhere('status', 'Pending')->get(); 
+                                                                        $cleanerID = Assigned_cleaner::Where('booking_id', $cleanerWithSchedule->booking_id)->Where('status', 'Accepted')->orWhere('status', 'Declined')->orWhere('status', 'Pending')->get(); 
                                                                     ?>
                                                                     @if($cleanerID != null) <!-- Check if booking already have a cleaner-->
                                                                         @foreach($cleaner_data as $key => $cleaner)
