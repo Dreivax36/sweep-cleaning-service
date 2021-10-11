2163422490703aad0afa18048def677c36d0ad09
Lyka Casilao <lycasilao/2gbox.adnu.edu.ph>
2021-10-11 09:04:04 +0800

    Oct 11 version 7

--- resources/views/customer/customer_transaction.blade.php
+++ resources/views/customer/customer_transaction.blade.php
@@ -7,6 +7,7 @@
     use App\Models\Cleaner;
     use App\Models\Assigned_cleaner;
     use App\Models\Review;
+    use App\Models\Payment;
 ?>
 
 @extends('customer/customer-nav/head_extention_customer-transactions') 
@@ -349,22 +350,15 @@
                 // This function captures the funds from the transaction.
                 return actions.order.capture().then(function(details) {
                 // This function shows a transaction success message to your buyer.
-                var booking_id = '{{$value->booking_id}}';
-                var amount  = '{{ $price_data->price }}';
-                $.ajax({
-                    method: "POST",
-                    url: "/checkout",
-                    data: {
-                        'booking_id': booking_id,
-                        'paypal_id': details.id,
-                        'amount': amount,
-                        'payment_mode':  'Paypal'
-                    },
-                    success: function (response) {
-                        swal(response.status);
-                        window.location.href = "/customer/customer_transaction";
-                    }
-                });
+                <?php 
+                    $id = '<script>document.writeln(details.id);</script>';
+                    $payments = new Payment();
+                    $payments->booking_id = $value->booking_id;
+                    $payments->amount = $price_data->price;
+                    $checkout = $payments->save();
+                    $checkout= Booking::Where('booking_id', $booking_id )->update(['is_paid' => true, 'paypal_id' => $id]);
+                ?>
+                    alert("Payment succcessful");
                 });
             },
             onCancel: function(data) {
