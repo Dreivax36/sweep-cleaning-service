bdf437faa2f612d53584835e1554514d57ce264c
Lyka Casilao <lycasilao/2gbox.adnu.edu.ph>
2021-10-12 05:04:30 +0800

    Oct 11 version 11

--- resources/views/emails/sendmail.blade.php
+++ resources/views/emails/sendmail.blade.php
@@ -11,12 +11,12 @@
   <h2> Hello! </h2>
   <p> Please click the button below to verify your email address. Thank you!</p>
   <div class="d-flex justify-content-center">
-    <button class="btn btn-primary" onclick="location.href='sweep-cleaning-service.herokuapp.com/verify/{{ $user_id }}'">Verify Email Address</button>
+    <button class="btn btn-primary" onclick="location.href='sweep-cleaning-service.herokuapp.com/verify/{{ $details['user_id'] }}'">Verify Email Address</button>
   </div>
   <p> Regards,</p>
   <p> Sweep Cleaning Service Team </p>
   <br><br>
-  <p> If you're having trouble clicking the "Verify Email Address" button, click the link  https://sweep-cleaning-service.herokuapp.com/verify/{{ $user_id }}
+  <p> If you're having trouble clicking the "Verify Email Address" button, click the link  https://sweep-cleaning-service.herokuapp.com/verify/ $details['user_id']
 </p> 
 </body>
 
