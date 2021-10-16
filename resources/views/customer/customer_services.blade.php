175714b5d2a371676eedb9f74bd7fa478c01c489
Lyka Casilao <lycasilao/2gbox.adnu.edu.ph>
2021-10-17 02:58:25 +0800

    October 17 version 5

--- resources/views/customer/customer_services.blade.php
+++ resources/views/customer/customer_services.blade.php
@@ -296,56 +296,55 @@ use App\Models\Service_review;
                             </div> <!-- End of Modal Content -->
                         </div>
                     </div>
-                    <div class="modal fade" id="success" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-                    <div class="modal-dialog modal-dialog-centered" role="document">
-                        <div class="modal-content">
-                        <div class="modal-body">
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">×</span>
-                            </button>
-                            <div class="icon">
-                                <i class="fa fa-check"></i>
-                            </div>
-                            <div class="title">
-                                Booking Success!
-                            </div>
-                            <div class="description">
-                                Thank You For Booking. We will notify you for
-                            </div>
-                            <div class="description">
-                                updates regarding the details of your transaction.
-                            </div>
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                        </div>
-                        </div>
-                    </div>
-                    </div>
-                    <div class="modal fade" id="error" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-                    <div class="modal-dialog modal-dialog-centered" role="document">
-                        <div class="modal-content">
-                        <div class="modal-body">
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">×</span>
-                            </button>
-                            <div class="icon">
-                                <i class="fa fa-times-circle"></i>
-                            </div>
-                            <div class="title">
-                                Something went wrong, try again.
-                            </div>
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                        </div>
-                        </div>
-                    </div>
-                    </div>
                     </div>                       
                 </div>
             </div>
         </div>
+        <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+        <div class="modal-body">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">×</span>
+            </button>
+            <div class="icon">
+                <i class="fa fa-check"></i>
+            </div>
+            <div class="title">
+                Booking Success!
+            </div>
+            <div class="description">
+                Thank You For Booking. We will notify you for
+            </div>
+            <div class="description">
+                updates regarding the details of your transaction.
+            </div>
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        </div>
+        </div>
+    </div>
+    </div>
+    <div class="modal fade" id="error" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+        <div class="modal-body">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">×</span>
+            </button>
+            <div class="icon">
+                <i class="fa fa-times-circle"></i>
+            </div>
+            <div class="title">
+                Something went wrong, try again.
+            </div>
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        </div>
+        </div>
+    </div>
+    </div>
         @endforeach
     </div>
     <?php
