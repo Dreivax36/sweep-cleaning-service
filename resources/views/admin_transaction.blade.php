a9fde31f62b5a8cb3f62211c62c79d9f746d45bc
Lyka Casilao <lycasilao/2gbox.adnu.edu.ph>
2021-10-20 15:56:04 +0800

    Oct 20 version 11

--- resources/views/admin_transaction.blade.php
+++ resources/views/admin_transaction.blade.php
@@ -326,7 +326,7 @@
                                
                                 ?>
                                 <div class="modal-footer trans_modal_footer">
-                                    @if($value->status == "Pending" && $declinecount == $price_data->number_of_cleaner)
+                                    @if($value->status == "Pending" && $declinecount == $price_data->number_of_cleaner && $statuscount != $price_data->number_of_cleaner)
                                         <button  type="submit" class="btn btn-block btn-primary on_progress_btn" name="status" value="No-Available-Cleaner">
                                             NO AVAILABLE CLEANER
                                         </button>
