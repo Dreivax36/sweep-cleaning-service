b4b4a04155806282f97bb47b94ca71343af75026
Lyka Casilao <lycasilao/2gbox.adnu.edu.ph>
2021-11-19 21:04:47 +0800

    Nov 19 ver 6

--- resources/views/admin_payroll_employee.blade.php
+++ resources/views/admin_payroll_employee.blade.php
@@ -142,7 +142,7 @@
         
                     <?php
                         $month = Carbon\Carbon::now()->month;
-                        $salary = Salary::selectRaw(' sum(totalHour) as totalHour, sum(totalDay) as totalDay, sum(totalsalary) as totalsalary, sum(totaltax) as totaltax, sum(netpay) as netpay')
+                        $salary = Salary::selectRaw('employee_code, created_at, sum(totalHour) as totalHour, sum(totalDay) as totalDay, sum(totalsalary) as totalsalary, sum(totaltax) as totaltax, sum(netpay) as netpay')
                         ->whereMonth('created_at', $month)
                             ->groupby('employee_code')
                             ->orderby('month', 'DESC')->get();
