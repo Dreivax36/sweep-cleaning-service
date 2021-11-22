d1f4e8245745f0ea43e39798cac87417b1a3b73e
Lyka Casilao <lycasilao/2gbox.adnu.edu.ph>
2021-11-22 14:21:35 +0800

    November 22 version 21

--- resources/views/admin_user_cleaner.blade.php
+++ resources/views/admin_user_cleaner.blade.php
@@ -82,7 +82,6 @@ use App\Models\Employee;
 </div>
 
 <body>
-    <div class="body-container">
         <?php
         $user_data = User::all();
         $user_count = User::all()->count();
@@ -361,6 +360,6 @@ use App\Models\Employee;
                 </div>
             </div>
         </div>
-    </div>
+
 </body>
 @endsection
