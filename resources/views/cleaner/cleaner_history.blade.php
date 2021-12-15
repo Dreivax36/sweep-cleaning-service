1c4d241649fefa9b9cc77218685da691f8bc5cac
Lyka Casilao <lycasilao/2gbox.adnu.edu.ph>
2021-12-15 23:56:41 +0800

    December 15 version 19

--- resources/views/cleaner/cleaner_history.blade.php
+++ resources/views/cleaner/cleaner_history.blade.php
@@ -11,6 +11,9 @@
     use App\Models\Service_review;
     use App\Models\Cleaner_review;
 ?>
+@extends('cleaner/cleaner-nav/head_extention_cleaner-history')
+
+@section('content')
 <title>
     Cleaner History Page
 </title>
@@ -342,7 +345,7 @@
         </div>
     </footer>
 </body>
-
+@endsection
 <script type="text/javascript">
 function getPageList(totalPages, page, maxLength){
   function range(start, end){
