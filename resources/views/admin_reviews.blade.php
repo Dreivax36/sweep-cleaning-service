a6f021b21272529bf6b9325b83a0ed236e119da9
Lyka Casilao <lycasilao/2gbox.adnu.edu.ph>
2022-01-17 17:26:17 +0800

    January 17 version 7

--- resources/views/admin_reviews.blade.php
+++ resources/views/admin_reviews.blade.php
@@ -93,10 +93,11 @@
         $price_end = Price::Where('property_type', 'Medium-Upper Class Residential Areas')->Where('service_id', $service_id)->value('price');
     ?>
     <div class="modal-header customer_services_modal_header">
+        
+        <div class="p-4">
         <button class="navbar-toggler" onclick="history.back()">
                 <i class="fas fa-arrow-left"></i>
         </button>
-        <div class="p-4">
             <h4 class="modal_customer_services_title">
                 {{$servicename}}
             </h4>
